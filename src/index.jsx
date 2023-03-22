4c97fadd98ab3145994febc1a96777e56b99d852
kal13 <lkallee@Abstract-technology.de>
2023-03-22 15:55:54 +0200

    updated home page

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,46 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import "babel-polyfill";
 
 import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
-} from '@edx/frontend-platform';
-import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-import ReactDOM from 'react-dom';
+  APP_INIT_ERROR,
+  APP_READY,
+  subscribe,
+  initialize,
+} from "@edx/frontend-platform";
+import { AppProvider, ErrorPage } from "@edx/frontend-platform/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch } from "react-router-dom";
 
-import Header, { messages as headerMessages } from '@edx/frontend-component-header';
-import Footer, { messages as footerMessages } from '@edx/frontend-component-footer';
 
-import appMessages from './i18n';
-import ExamplePage from './example/ExamplePage';
+import appMessages from "./i18n";
+import LandingPage from "./Components/Landing_page/Landing_page";
 
-import './index.scss';
+import "./index.scss";
+import NotFoundPage from "./Components/NotFoundPage";
+// import About from "./Components/Course_About/Course_about";
 
 subscribe(APP_READY, () => {
   ReactDOM.render(
     <AppProvider>
-      <Header />
-      <ExamplePage />
-      <Footer />
+      <main>
+        <Switch>
+          <Route path="*" component={LandingPage} />
+          {/* <Route path="/courses/:courseId/about" component={About} /> */}
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
+      </main>
     </AppProvider>,
-    document.getElementById('root'),
+    document.getElementById("root")
   );
 });
 
 subscribe(APP_INIT_ERROR, (error) => {
-  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+  ReactDOM.render(
+    <ErrorPage message={error.message} />,
+    document.getElementById("root")
+  );
 });
 
 initialize({
-  messages: [
-    appMessages,
-    headerMessages,
-    footerMessages,
-  ],
+  messages: [appMessages],
 });
