77d5700c97e3a727b2b4ec13e3a15174dded2003
kal13 <lkallee@Abstract-technology.de>
2023-04-26 13:37:31 +0200

    updated course link

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -48,7 +48,7 @@ const CourseCard = (data) => {
           </div>
         </div>
       </a> */}
-      <Hyperlink destination={"/courses/course-v1:"+ data.name +"/about"}>
+      <Hyperlink destination={"/courses/course-v1:"+ data.id +"/about"}>
       <Card isClickable>
       <div className="top-btn-container">
             <button type="button" className="cost-tag-btn btn btn-primary free">
